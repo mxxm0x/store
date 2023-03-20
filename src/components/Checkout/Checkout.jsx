6e09fef5c55aab77855738a04ef5224f929df30f
Rapha Pouso <raphamoreirapouso@gmail.com>
2023-03-20 06:32:45 -0300

    pulando linha

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -9,23 +9,24 @@ function Checkout() {
 					<p className="header-text">You're paying,</p>
 					<h1 className="header-value">$450.00</h1>
 				</div>
+				<div className="all">
+					<div className="products">
+						<p className="title">Custom Gucci Shoes</p>
+						<p className="sub1">Size: m Color: Red</p>
+						<p className="title2">Nivea Cream 400ml</p>
+						<p className="sub2">Size: m Color: Blue</p>
+						<p className="discounts">Discounts & Offers</p>
+						<p className="tax">Tax</p>
+						<p className="final">Total</p>
+					</div>
 
-				<div className="products">
-					<p className="title">Custom Gucci Shoes</p>
-					<p className="sub1">Size: m Color: Red</p>
-					<p className="title2">Nivea Cream 400ml</p>
-					<p className="sub2">Size: m Color: Blue</p>
-					<p className="discounts">Discounts & Offers</p>
-					<p className="tax">Tax</p>
-					<p className="final">Total</p>
-				</div>
-
-				<div className="costs">
-					<p className="value">$400.00</p>
-					<p className="value1">$50.00</p>
-					<p className="value2">$0.00</p>
-					<p className="value3">$0.00</p>
-					<p className="value4">$450.00</p>
+					<div className="costs">
+						<p>$400.00</p>
+						<p>$50.00</p>
+						<p>$0.00</p>
+						<p>$0.00</p>
+						<p>$450.00</p>
+					</div>
 				</div>
 			</div>
 		</div>
