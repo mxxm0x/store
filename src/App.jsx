f4b4560468c58b105db74c8c3aa0e283f0ecee43
Rapha Pouso <raphamoreirapouso@gmail.com>
2023-03-20 02:45:32 -0300

    mudancas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Checkout from './components/Checkout/Checkout';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Checkout />
+		</div>
+	);
 }
 
 export default App;
