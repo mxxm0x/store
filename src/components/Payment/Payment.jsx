c4e81b632e78322a715c67368f1be41eea8fab4a
C1330938 AronRibeiro <aronr.chaves@ibm.com>
2023-03-20 05:44:20 -0300

    add: payment component using several components

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import './Payment.css';
 import Input from './Input';
-
+import Button from './Button';
 function Payment(props) {
 	return (
 		<div className="container">
-			<div className="header">
-				<h1 className="headerText">Let's Make Payment</h1>
-				<p className="call-to-card">
-					To start your subscription, input your card details to make payment.
-					You will be redirected to your banks authorization page .{' '}
-				</p>
-			</div>
+			<h1 className="headerText">Let's Make Payment</h1>
+			<p className="call-to-card">
+				To start your subscription, input your card details to make payment. You
+				will be redirected to your banks authorization page .{' '}
+			</p>
 
-			<Input />
+			<label className="lholder-name">Cartholder's name</label>
+			<Input place={'PAULINA CHIMAROKE'} className={'Pinput'} />
+
+			<label className="lcard-number">Card Number</label>
+			<Input place={'9870 3456 7890 6473'} className={'card-number'} />
+
+			<label className="lexpiration-date">Expiration Date</label>
+			<Input place={'03/30'} className={'expire-date'} />
+
+			<label className="lcvc">CVC</label>
+			<Input place={'654'} className={'card-cvc'} />
+
+			<label className="lcoupoun">Coupon</label>
+			<span className="apply">Apply</span>
+			<Input place={'CHIKAMSO-20-OFF'} className={'card-coupon'} />
+			<Button />
 		</div>
 	);
 }
