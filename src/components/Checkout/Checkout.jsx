3d4f18153977ae103b49561736662f8fc77e9018
Rapha Pouso <raphamoreirapouso@gmail.com>
2023-03-20 04:45:08 -0300

    textos adicionados

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -5,11 +5,27 @@ function Checkout() {
 	return (
 		<div className="cContainer">
 			<div className="background">
-				<p className="first">You're paying,</p>
-				<h1 className="value">$450.00</h1>
+				<div className="header">
+					<p className="header-text">You're paying,</p>
+					<h1 className="header-value">$450.00</h1>
+				</div>
+				<div className="products">
+					<p className="title">Custom Gucci Shoes</p>
+					<p className="sub1">Size: m Color: Red</p>
+					<p className="title2">Nivea Cream 400ml</p>
+					<p className="sub2">Size: m Color: Blue</p>
+					<p className="discounts">Discounts & Offers</p>
+					<p className="tax">Tax</p>
+					<p className="tax">Total</p>
 
-				<p className="title">Custom Gucci Shoes</p>
-				<p className="title2">Nivea Cream 400ml</p>
+					<div className="costs">
+						<p className="value">$400.00</p>
+						<p className="value1">$50.00</p>
+						<p className="value2">$0.00</p>
+						<p className="value3">$0.00</p>
+						<p className="value4">$450.00</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
