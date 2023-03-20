739a49ed4a1ff0508a491da1cb0cb57611995466
Rapha Pouso <raphamoreirapouso@gmail.com>
2023-03-20 04:07:50 -0300

    tentando colocar os textos

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -7,6 +7,9 @@ function Checkout() {
 			<div className="background">
 				<p className="first">You're paying,</p>
 				<h1 className="value">$450.00</h1>
+
+				<p className="title">Custom Gucci Shoes</p>
+				<p className="title2">Nivea Cream 400ml</p>
 			</div>
 		</div>
 	);
