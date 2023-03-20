363ec29ca780af7e276ef3d534a02fa3e3e6c0ef
C1330938 AronRibeiro <aronr.chaves@ibm.com>
2023-03-20 03:03:34 -0300

    working

---
+++ src/components/Payment/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Input() {
+	return (
+		<div className="card-info">
+			<p className="holder-name">Cardholder's name</p>
+			<input type="text" className="Pinput" placeholder="EXAMPLE NAME" />
+		</div>
+	);
+}
+
+export default Input;
--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Payment.css';
+import Input from './Input';
 
-function Payment() {
+function Payment(props) {
 	return (
 		<div className="container">
-			<h1 className="headerText">Let's Make Payment</h1>
-			<p className="call-to-card">
-				To start your subscription, input your card details to make payment. You
-				will be redirected to your banks authorization page .{' '}
-			</p>
+			<div className="header">
+				<h1 className="headerText">Let's Make Payment</h1>
+				<p className="call-to-card">
+					To start your subscription, input your card details to make payment.
+					You will be redirected to your banks authorization page .{' '}
+				</p>
+			</div>
+
+			<Input />
 		</div>
 	);
 }
