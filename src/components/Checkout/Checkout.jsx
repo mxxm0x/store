c5dd1d8a7c0be8db866c66a4e5f77785e50baed8
Rapha Pouso <raphamoreirapouso@gmail.com>
2023-03-20 03:07:08 -0300

    tentando alinhar

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Checkout.css';
+
 function Checkout() {
-	return;
-	<div className="cContainer">
-		<div className="background"></div>
-	</div>;
+	return (
+		<div className="cContainer">
+			<div className="background">
+				<p>bunda</p>
+			</div>
+		</div>
+	);
 }
 
 export default Checkout;
